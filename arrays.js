9e3d846eed240a375183859b35ed367b1526cb73
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2019-12-04 20:05:14 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ["snicers", "hundred grand", "kitkat", "skittels"];
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+   array.unshift(element);
+   return array;
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
